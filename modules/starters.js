6643f4c15443851ad747a7677acf313488630d9d
Sid_KH <my.mail.kharkov@gmail.com>
2016-02-15 23:12:38 +0200

    Coloring logs

--- modules/starters.js
+++ modules/starters.js
@@ -1,5 +1,18 @@
-var inquirer = require('inquirer');
-// Todo make all question one array
+var inquirer = require('inquirer'),
+  colors = require('colors');
+
+colors.setTheme({
+  info: ['white', 'bgBlack'],
+  success: ['green', 'bgBlack'],
+  warn: ['orange', 'bgBlack'],
+  error: ['red', 'bgBlack']
+});
+
+function logComplete() {
+  console.log('[Complete]'.success);
+  console.log('\n\n');
+}
+
 module.exports = function () {
   var qTypes = [
     {
@@ -13,7 +26,9 @@ module.exports = function () {
             repo: 'https://github.com/SidKH/angular-starter-kit.git',
             branch: 'master',
             exec: function () {
+              console.log('Installing Npm and Bower dependencies'.info);
               exec('npm install');
+              logComplete();
             }
           }
         },
@@ -23,14 +38,16 @@ module.exports = function () {
             repo: 'https://github.com/SidKH/markup-starter',
             branch: 'markup',
             exec: function () {
+              console.log('Installing Npm and Bower dependencies'.info);
               exec('npm install');
+              logComplete();
             }
           }
         }
       ]
     },
     {
-      message: 'Type project repository (default: no repository)',
+      message: 'Type project repository (Bitbucket JustCoded)',
       type: 'input',
       name: 'repo'
     },
@@ -51,17 +68,24 @@ module.exports = function () {
 
   function commands(info) {
     if (!info.type) {
-      console.log('Still in maintenance, sorry');
+      console.log('Still in maintenance, sorry'.red);
       return;
     }
+    console.log('Getting starter files'.info);
     exec('git clone ' + info.type.repo + ' ./');
+    logComplete();
     exec('rm -rf .git');
     if (info.repo) {
       info.branch = info.branch || 'master';
+      console.log('Create initial commit and push it into repository'.info);
       exec('git init && git add . && git commit -m "Init Starter Kit"');
+      logComplete();
       exec('git remote add origin ' + info.repo);
+      console.log('Pushing the code to the repository'.info);
       exec('git push origin ' + info.branch);
+      logComplete();
     }
     info.type.exec && info.type.exec();
+    console.log('Starter was successfully installed. Good luck :)'.success);
   }
 };
